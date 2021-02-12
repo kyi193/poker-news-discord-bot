595e3a450298e5aef929ac41398eeb05b2136c01
Kevin <kevin.yi93@gmail.com>
2021-02-12 18:59:57 -0500

    Replace api endpoint with google search api and modify json res

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,18 +1,39 @@
 const express = require("express");
 const router = express.Router();
 const Axios = require("axios");
-
-const API_URL = "http://hn.algolia.com/api/v1/search_by_date?query=poker?tags=story";
+require('dotenv').config({path:'../.env' });
 
 router.get("/welcome", function (req, res, next) {
   res.status(200).send({ welcomeMessage: "Step 1 (completed)" });
 });
 
-router.get("/search", function (req, res, next) {
-  Axios.get(API_URL)
+router.get("/search", function (req, res) {
+  const request = {
+    method: 'get',
+    url: 'https://google-search3.p.rapidapi.com/api/v1/news/q=poker?num=100',
+    headers: {
+      'x-rapidapi-key': process.env.RAPID_API_KEY,
+      'x-rapidapi-host': 'google-search3.p.rapidapi.com',
+      'useQueryString': true
+    },
+    json: true,
+    gzip: true,
+  };
+
+  Axios(request)
       .then(result => {
-          const { hits } = result.data;
-          res.status(200).send(hits);
+        const { entries } = result.data;
+        const articles = [];
+
+        for(const entry of entries) {
+          articles.push({
+            id: entry.id,
+            title: entry.title,
+            link: entry.link,
+            published: entry.published
+          })
+        }
+        res.status(200).send(articles);
       })
       .catch(error => console.error(error));
 });
